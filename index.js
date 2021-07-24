b07fa27dac5c1a3cf19314a4e99edf7d0bf6dc86
lashewi <swisum@gmail.com>
2021-07-25 00:23:49 +0530

    feat: initial

--- index.js
+++
@@ -1,8 +0,0 @@
-function changeTheme() {
-    var classList = document.body.classList;
-    if (classList.length > 0) {
-        classList.remove('dark-theme');
-    } else {
-        classList.add('dark-theme');
-    }   
-}
